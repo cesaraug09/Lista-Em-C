649582ffe3b3fec54b03c1d5743df3418e59d880
cesar <cesar.caju@academico.ifpb.edu.br>
2024-05-02 12:11:18 -0300

    mais questoes

--- Comandos entrada e saída/exec014.c
+++ Comandos entrada e saída/exec014.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
-//Escreva um programa que leia uma palavra e calcule o seu comprimento. 
+//Escreva um programa que leia uma palavra e calcule o seu comprimento.
+
+int main(){
+    char palavra[100];
+    int cont=0;
+    printf("Escreva uma palavra:");
+    scanf("%s",&palavra);
+    while(palavra[cont]!='\0'){
+        cont++;
+    }
+    printf("\nA palavra %s tem %d letras.",palavra, cont);
+    return 0;
+}
--- Comandos entrada e saída/exec015.c
+++ Comandos entrada e saída/exec015.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 //Escreva um programa que leia uma palavra e um número inteiro k e identifique a k ésima letra da palavra informada pelo usuário.
+
+int main(){
+    int n;
+    char palavra[100];
+    printf("Escreva uma palavra:");
+    scanf("%s",palavra);
+    printf("Agora digite a posicao da letra:");
+    scanf("%d",&n);
+    printf("\nA %d letra da palavra %s eh %c",n, palavra, palavra[n-1]);
+    return 0;
+}
